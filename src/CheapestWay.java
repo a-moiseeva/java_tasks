b0e3efadc2fe2d9030e83ef1c940711b91ff4282
a_moiseeva <a.moiseeva.fki@gmail.com>
2023-09-27 23:40:35 +0300

    added CheapestWay

--- src/CheapestWay.java
+++ src/CheapestWay.java
@@ -10,64 +10,64 @@ public class CheapestWay {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(reader.readLine());
         String[] first = reader.readLine().split(" ");
-        int m= first.length;
-
+        int n = Integer.parseInt(first[0]);
+        int m = Integer.parseInt(first[1]);
         int[][] a = new int[n][m];
 
-        for (int i =0;i<m;i++){
-            a[0][i]=Integer.parseInt(first[i]);
-        }
-        for (int i=1;i<n;i++){
+        for (int i = 0; i < n; i++) {
             first = reader.readLine().split(" ");
-            for (int j =0;j<m;j++){
-                a[i][j]=Integer.parseInt(first[j]);
+            for (int j = 0; j < m; j++) {
+                a[i][j] = Integer.parseInt(first[j]);
             }
         }
 
+        System.out.println("finish");
 
-        int x=n-1;
-        int y=m-1;
+        int r,d,x,y;
 
-        while (x!=0 && y!=0){
-            if(x!=0){
-                x--;
+        int xx = n - 1 ;
+        int yy = m - 1;
+
+
+        while ((xx != 0) || (yy != 0)) {
+            System.out.println("big cycle "+xx+" "+yy);
+            if (xx != 0) {
+                xx--;
+            } else if (xx == 0) {
+                yy--;
             }
-            if(x==0){
+            x=xx;
+            y=yy;
+            while ((x != n) && (y != -1)) {
+                System.out.println("lil cycle "+x+" "+y);
+                if (x==n-1){
+                    a[x][y]=a[x][y]+a[x][y+1];
+                } else if (y==m-1){
+                    a[x][y]=a[x][y]+a[x+1][y];
+                } else{
+                    d=a[x][y+1];
+                    r=a[x+1][y];
+                    if(d<=r){
+                        a[x][y]=a[x][y]+d;
+                    }else{
+                        a[x][y]=a[x][y]+r;
+                    }
+                }
+                x++;
                 y--;
             }
-            while(x!=0 || y!=m){
-                try{
-                    a[x][y]=a[x][y]+a[x][y+1];
-                }catch(Exception e){
-
-                }
-                try{
-                    a[x][y]=a[x][y]+a[x+1][y];
-                }catch(Exception e){
-
-                }
-                x--;
-                y++;
-            }
         }
 
-        for (int i=0;i<n;i++){
-            for (int j=0;j<m;j++){
-                System.out.print(a[i][j]+" ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(a[i][j] + " ");
             }
             System.out.println(" ");
 
         }
 
-        System.out.println(a[0][0]);
-
-
-
-
-
-
+        System.out.println("answer: "+ a[0][0]);
 
 
         reader.close();
